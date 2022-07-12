989325f21f2b56e27e94168cae40dbd0e21b1c1a
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-12 08:13:36 -0700

    handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -6,7 +6,7 @@
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @width: get width.
+ * @width: Get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
