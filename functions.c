b5ea44a05e3082ca499da3d44998173690aad86f
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-12 08:26:23 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -7,7 +7,7 @@
  * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates Active flags
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
