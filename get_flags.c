cafbca0b8a7816662d0ef38ee44bff82a3b8dd30
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-12 09:54:44 -0700

    printf_new_update

--- get_flags.c
+++ get_flags.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_flags - Calculates the active flags
  * @format: formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
